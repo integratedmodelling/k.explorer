246b823ba829b09709c41fc5eacf691d3ffe5f78
Enrico Girotto <enrico.girotto@gmail.com>
2018-08-28 10:27:17 +0200

    Added other possible SVG but commented

--- src/shared/MapConstants.js
+++ src/shared/MapConstants.js
@@ -27,9 +27,27 @@ export const MAP_CONSTANTS = {
 };
 
 export const MAP_ELEMENTS = {
-  MARKER_SVG: ({ fill = 'white', stroke = 'black', strokeWidth = '5px' }) => `<svg width="85" height="85" version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <path style="fill: ${fill}; stroke: ${stroke}; stroke-width: ${strokeWidth}" d="M40,0C26.191,0,15,11.194,15,25c0,23.87,25,55,25,55s25-31.13,25-55C65,11.194,53.807,0,40,0z 
+  MARKER_SVG: ({ fill = 'white', stroke = 'black', strokeWidth = '5px' }) => `<svg width="80" height="80" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+  <path class="svg-marker" style="fill: ${fill}; stroke: ${stroke}; stroke-width: ${strokeWidth}" d="M40,0C26.191,0,15,11.194,15,25c0,23.87,25,55,25,55s25-31.13,25-55C65,11.194,53.807,0,40,0z
   M40,38.8c-7.457,0-13.5-6.044-13.5-13.5S32.543,11.8,40,11.8c7.455,0,13.5,6.044,13.5,13.5S47.455,38.8,40,38.8z"/></svg>`,
+  /*
+    `<?xml version="1.0" encoding="iso-8859-1"?>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" width="40" height="40" x="0px" y="0px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+  <g>
+    <path style="fill:${fill};" d="M19.999,38.251C17.896,35.874,7.5,23.628,7.5,14C7.5,7.107,13.107,1.5,20,1.5S32.5,7.107,32.5,14   C32.5,23.58,22.102,35.866,19.999,38.251z"/>
+    <g>
+      <path style="fill:${stroke};" d="M20,2c6.617,0,12,5.383,12,12c0,8.932-9.29,20.349-12.001,23.491C17.286,34.358,8,22.977,8,14    C8,7.383,13.383,2,20,2 M20,1C12.82,1,7,6.82,7,14c0,10.981,13,25,13,25s13-14.074,13-25C33,6.82,27.18,1,20,1L20,1z"/>
+    </g>
+  </g>
+  <g>
+    <circle fill-opacity="0" cx="20" cy="14" r="5.5"/>
+    <g>
+      <path style="fill:${stroke};" d="M20,9c2.761,0,5,2.239,5,5s-2.239,5-5,5s-5-2.239-5-5S17.239,9,20,9 M20,8c-3.308,0-6,2.692-6,6    s2.692,6,6,6s6-2.692,6-6S23.308,8,20,8L20,8z"/>
+    </g>
+  </g>
+  </svg>`,
+  */
+
 };
 
 export const MAP_STYLE_ELEMENTS = {
