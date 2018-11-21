d898a50f594a45a89b188203aeccebccb3971a63
Enrico Girotto <enrico.girotto@gmail.com>
2018-11-21 15:15:49 +0100

    Some movements

--- src/components/DataflowViewer.vue
+++ src/components/DataflowViewer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fit no-padding"  @click.native="change">
+  <div class="fit no-padding" id="dfv-container">
     <div id="sprotty"></div>
   </div>
 </template>
@@ -7,7 +7,8 @@
 <script>
 import { mapGetters } from 'vuex';
 // eslint-disable-next-line object-curly-newline
-import { createContainer, TYPES, FitToScreenAction, UpdateModelAction, ElkGraphJsonToSprotty, LocalModelSource } from 'klab-elk-sprotty-bridge/lib';
+import { createContainer, TYPES, FitToScreenAction, UpdateModelAction, ElkGraphJsonToSprotty } from 'klab-elk-sprotty-bridge/lib';
+import ELK from 'elkjs';
 import { Helpers } from '../shared/Helpers';
 
 export default {
@@ -17,7 +18,8 @@ export default {
       graph: undefined, // the sprotty dataflow
       modelSource: undefined,
       actionDispatcher: undefined,
-      elkToSprotty: undefined,
+      elkGraphJsonToSprotty: undefined,
+      elk: undefined,
       interval: null,
     };
   },
@@ -29,13 +31,11 @@ export default {
   methods: {
     initGraph() {
       if (this.dataflow !== null) {
-        this.$ELK.layout(this.dataflow).then((graph) => {
-          this.graph = new ElkGraphJsonToSprotty().transform(graph);
+        this.elk.layout(this.dataflow).then((graph) => {
+          this.graph = this.elkGraphJsonToSprotty.transform(graph);
           this.modelSource.setModel(this.graph);
           this.actionDispatcher.dispatch(new FitToScreenAction([]));
         });
-      } else {
-        this.graph = null;
       }
     },
     /*
@@ -80,13 +80,15 @@ export default {
       }
     },
   },
+  created() {
+    this.elk = new ELK();
+    this.elkGraphJsonToSprotty = new ElkGraphJsonToSprotty();
+  },
   mounted() {
     // Create Sprotty viewer
-    const sprottyContainer = createContainer();
-    sprottyContainer.bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
+    const sprottyContainer = createContainer('info');
     this.modelSource = sprottyContainer.get(TYPES.ModelSource);
     this.actionDispatcher = sprottyContainer.get(TYPES.IActionDispatcher);
-    this.elkToSprotty = new ElkGraphJsonToSprotty();
     this.initGraph();
     setInterval(() => this.changeModel(), 1000);
   },
@@ -97,72 +99,50 @@ export default {
 };
 </script>
 
-<style>
-  #sprotty {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-  .elknode {
-    stroke: #448;
-    stroke-width: 1;
-    fill: #ddf;
-  }
-
-  .elkport {
-    stroke: #448;
-    stroke-width: 1;
-    fill: #66c;
-  }
-
-  .elkedge {
-    fill: none;
-    stroke: #224;
-    stroke-width: 1;
-  }
-
-  .elkedge.arrow {
-    fill: #336;
-  }
-
-  .elklabel {
-    stroke-width: 0;
-    stroke: #000;
-    fill: #000;
-    font-family: sans-serif;
-    font-size: 10pt;
-    dominant-baseline: hanging;
-  }
-
-  .elkjunction {
-    stroke: none;
-    fill: #224;
-  }
-
-  .selected > rect {
-    stroke-width: 3;
-  }
-
-  .mouseover {
-    stroke: #88f;
-  }
-
-  .waiting {
-    fill: #ffab32;
-  }
-  .processed {
-    fill: #0f0;
-  }
-
-  .processing {
-    fill: #f00;
-  }
-
-
+<style lang="stylus">
+  #dfv-container
+    #sprotty
+      font-family Roboto
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom 0
+      svg
+        width 100%
+        height 100%
+        .elknode
+          stroke #448
+          stroke-width 1
+          fill #ddf
+        .elkport
+          stroke #448
+          stroke-width 1
+          fill #66c
+        .elkedge
+          fill none
+          stroke #224
+          stroke-width 1
+        .elkedge.arrow
+          fill #336
+        .elklabel
+          stroke-width 0
+          stroke #000
+          fill #000
+          font-family Roboto
+          font-size 10pt
+          dominant-baseline hanging
+        .elkjunction
+          stroke none
+          fill #224
+        .selected > rect
+          stroke-width 3
+        .mouseover
+          stroke #88f
+        .waiting
+          fill #ffab32
+        .processed
+          fill #0f0
+        .processing
+          fill #f00
 </style>
