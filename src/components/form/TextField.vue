efd54cc9fd2338da0edba907b8e5b32b110f90ad
Enrico Girotto <enrico.girotto@gmail.com>
2019-04-08 10:07:10 +0200

    Start create component for KEX-37

---
+++ src/components/form/TextField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label :for="name">{{ description }}</label>
+    <q-input
+      color="mc-main"
+      :placeholder="initialValue"
+      :name="name"
+      type="text"
+      value=""
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'TextField',
+  props: ['initialValue', 'description', 'name'],
+};
+</script>
+<style>
+</style>
