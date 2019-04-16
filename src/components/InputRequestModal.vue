66e14247ec151341f90545b119b861139275824c
Enrico Girotto <enrico.girotto@gmail.com>
2019-04-16 16:30:39 +0200

    added identity to message

--- src/components/InputRequestModal.vue
+++ src/components/InputRequestModal.vue
@@ -58,6 +58,9 @@ export default {
     };
   },
   computed: {
+    ...mapGetters('data', [
+      'session',
+    ]),
     ...mapGetters('view', [
       'hasInputRequests',
       'inputRequests',
