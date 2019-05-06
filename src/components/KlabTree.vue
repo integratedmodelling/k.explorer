1b92c05e940d457fdd32f169f2bf46b1e1d04fb3
Enrico Girotto <enrico.girotto@gmail.com>
2019-05-06 10:54:32 +0200

    Remove the link for download resources

--- src/components/KlabTree.vue
+++ src/components/KlabTree.vue
@@ -255,6 +255,8 @@ export default {
           link.setAttribute('download', `${label}.${selectedFormat.extension}`);
           document.body.appendChild(link);
           link.click();
+          link.remove();
+          window.URL.revokeObjectURL(url);
           callback();
         },
       );
