64f858698e41da246fb111a2c3ee41de411d87d3
Enrico Girotto <enrico.girotto@gmail.com>
2019-05-23 13:10:08 +0200

    palliate a checkbox on observation info fail if observation is empty

--- src/components/ObservationInfo.vue
+++ src/components/ObservationInfo.vue
@@ -7,7 +7,7 @@
           :keep-color="true"
           color="mc-yellow"
           @click.native="showNode"
-          :disabled="observationInfo.valueCount === 1"
+          :disabled="observationInfo.valueCount === 1 || observationInfo.empty"
         ></q-checkbox>
       </div>
       <div id="oi-name" class="oi-control oi-text"><span>{{ observationInfo.label }}</span></div>
