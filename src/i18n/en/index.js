303d24260c17c3083491fd822c156b3fd3fedf6d
Enrico Girotto <enrico.girotto@gmail.com>
2019-09-13 10:36:53 +0200

    New section in main control menu

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -37,6 +37,7 @@ export default {
     mcMenuScale: 'Space & time:',
     mcMenuContext: 'Context',
     mcMenuOption: 'Options',
+    mcMenuSettings: 'Settings',
     titleOutputFormat: 'Download observation',
     askForOuputFormat: 'Select format',
     titleChangeScale: 'Change {type} scale',
