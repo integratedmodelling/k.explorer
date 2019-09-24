ea22c3236f27a9df19ed30dae2d138ed2ea46da5
Enrico Girotto <enrico.girotto@gmail.com>
2019-09-24 12:48:43 +0200

    Some sample

--- src/components/KlabPresentation.vue
+++ src/components/KlabPresentation.vue
@@ -3,15 +3,15 @@
     <q-carousel
       class="text-white"
       infinite
-      autoplay
       arrows
       color="white"
     >
       <q-carousel-slide
         class="flex flex-center"
-        :class="`bg-${colors[1]}`"
       >
-        <div class="q-display-3">Slide 1</div>
+        <div class="q-display-3">
+          <div class="kp-slide-title">Welcome to k.Explorer: the easiest way to navigate ARIES</div>
+        </div>
       </q-carousel-slide>
       <q-carousel-slide
         class="flex flex-center"
@@ -51,5 +51,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="stylus">
+  @import '~variables'
+
+  .kp-slide-title
+    font-size 1.2em
+    color $main-control-green
+    h1
+      color white
 </style>
