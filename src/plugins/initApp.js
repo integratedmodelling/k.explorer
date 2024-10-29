5348fafde5372ec19aeeac90d421fe34bb67306d
Kristina <kristina.sanchez@bc3research.org>
2024-10-29 15:40:59 +0100

    IM-468 fix: adapt login.js to Quasar 0.27 compatibility

--- src/plugins/initApp.js
+++ src/plugins/initApp.js
@@ -7,13 +7,9 @@ import Vue from 'vue';
 
 const eventBus = new Vue();
 
-console.debug('entro aki');
-console.log('entro aki');
-
 export default ({ store }) => {
   const urlParams = new URLSearchParams(window.location.search);
 
-  console.log(urlParams);
 
   // Session and mode
   const session = urlParams.get(WEB_CONSTANTS.PARAMS_SESSION)
@@ -31,7 +27,6 @@ export default ({ store }) => {
   const remoteDebug = urlParams.get(WEB_CONSTANTS.PARAMS_DEBUG_REMOTE);
   let local;
 
-  console.log(remoteDebug);
   if (remoteDebug) {
     local = remoteDebug !== 'true';
   } else {
--- src/plugins/login.js
+++ src/plugins/login.js
@@ -3,62 +3,64 @@ import authentication from './authentication';
 import { WEB_CONSTANTS, KEYCLOAK } from '../shared/Constants';
 
 
-const urlParams = new URLSearchParams(window.location.search);
+export default () => {
+    const urlParams = new URLSearchParams(window.location.search);
 
-console.log(urlParams);
-const remoteDebug = urlParams.get(WEB_CONSTANTS.PARAMS_DEBUG_REMOTE);
-console.log('hola');
-console.log(remoteDebug);
-
-
-Vue.config.productionTip = false;
-Vue.use(authentication);
+    console.log(urlParams);
+    const remoteDebug = urlParams.get(WEB_CONSTANTS.PARAMS_DEBUG_REMOTE);
+    console.log('hola');
+    console.log(remoteDebug);
 
 
-Vue.$keycloak
-    .init({ onLoad: 'login-required', checkLoginIframe: false })
-    .then((auth) => {
-        if (!auth) {
-            window.location.reload();
-        } else {
-            localStorage.setItem(KEYCLOAK.TOKEN, Vue.$keycloak.token);
-            localStorage.setItem(KEYCLOAK.REFRESH_TOKEN, Vue.$keycloak.refreshToken);
-            console.debug(Vue.prototype.$axios.defaults.headers);
-            Vue.prototype.$axios.defaults.headers.common.Authorization = KEYCLOAK.BEARER + Vue.$keycloak.token;
+    Vue.config.productionTip = false;
+    Vue.use(authentication);
 
-            console.debug('Authenticated');
-        }
 
-        setInterval(() => {
-            Vue.$keycloak.updateToken().then((refreshed) => {
-                // if (store.getters['auth/isLoggedIn'] !== false) {
-                if (refreshed) {
-                    console.debug(`Token refreshed ${refreshed}`);
-                    localStorage.setItem(KEYCLOAK.TOKEN, Vue.$keycloak.token);
-                    localStorage.setItem(KEYCLOAK.REFRESH_TOKEN, Vue.$keycloak.refreshToken);
-                    Vue.prototype.$axios.defaults.headers.common.Authorization = KEYCLOAK.BEARER + Vue.$keycloak.token;
-                } else {
-                    console.debug(`Token not refreshed, valid for ${Math.round(Vue.$keycloak.tokenParsed.exp + Vue.$keycloak.timeSkew - new Date().getTime() / 1000)} seconds`);
-                }
-                // }
-                // else {
-                //   // If the user is reading the agreement not logout
-                //   if (!store.getters['keycloak/signing_agreement']) {
-                //     // __ENV__.APP_BASE_URL
-                //     const logoutOptions = { redirectUri: 'http://localhost:8080/modeler' };
+    Vue.$keycloak
+        .init({ onLoad: 'login-required', checkLoginIframe: false, silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html` })
+        .then((auth) => {
+            if (!auth) {
+                window.location.reload();
+            } else {
+                localStorage.setItem(KEYCLOAK.TOKEN, Vue.$keycloak.token);
+                localStorage.setItem(KEYCLOAK.REFRESH_TOKEN, Vue.$keycloak.refreshToken);
+                console.debug(Vue.prototype.$axios.defaults.headers);
+                Vue.prototype.$axios.defaults.headers.common.Authorization = KEYCLOAK.BEARER + Vue.$keycloak.token;
 
-                //     Vue.$keycloak.logout(logoutOptions)
-                //       .catch((error) => {
-                //         console.error(error);
-                //       });
-                //     store.commit('LOGOUT');
-                //   }
-                // }
-            }).catch(() => {
-                console.error('Failed to refresh token');
-            });
-        }, 60000);
-    }).catch((error) => {
-        console.error(error);
-        console.debug('Authenticated Failed');
-    });
+                console.debug('Authenticated');
+            }
+
+            setInterval(() => {
+                Vue.$keycloak.updateToken().then((refreshed) => {
+                    // if (store.getters['auth/isLoggedIn'] !== false) {
+                    if (refreshed) {
+                        console.debug(`Token refreshed ${refreshed}`);
+                        localStorage.setItem(KEYCLOAK.TOKEN, Vue.$keycloak.token);
+                        localStorage.setItem(KEYCLOAK.REFRESH_TOKEN, Vue.$keycloak.refreshToken);
+                        Vue.prototype.$axios.defaults.headers.common.Authorization = KEYCLOAK.BEARER + Vue.$keycloak.token;
+                    } else {
+                        console.debug(`Token not refreshed, valid for ${Math.round(Vue.$keycloak.tokenParsed.exp + Vue.$keycloak.timeSkew - new Date().getTime() / 1000)} seconds`);
+                    }
+                    // }
+                    // else {
+                    //   // If the user is reading the agreement not logout
+                    //   if (!store.getters['keycloak/signing_agreement']) {
+                    //     // __ENV__.APP_BASE_URL
+                    //     const logoutOptions = { redirectUri: 'http://localhost:8080/modeler' };
+
+                    //     Vue.$keycloak.logout(logoutOptions)
+                    //       .catch((error) => {
+                    //         console.error(error);
+                    //       });
+                    //     store.commit('LOGOUT');
+                    //   }
+                    // }
+                }).catch(() => {
+                    console.error('Failed to refresh token');
+                });
+            }, 60000);
+        }).catch((error) => {
+            console.error(error);
+            console.debug('Authenticated Failed');
+        });
+};
